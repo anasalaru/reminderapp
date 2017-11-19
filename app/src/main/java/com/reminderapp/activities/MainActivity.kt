fa1ca5529662a437d014bc32c80ec0ddd2490ace
Ana Maria Salaru <anamaria.salaru@thinslices.com>
2017-11-19 18:51:56 +0200

    Change RecyclerView to ListView

--- app/src/main/java/com/reminderapp/activities/MainActivity.kt
+++ app/src/main/java/com/reminderapp/activities/MainActivity.kt
@@ -2,23 +2,28 @@ package com.example.anamaria.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import com.example.anamaria.reminderapp.R
 import com.reminderapp.data.DataManager
-import com.reminderapp.ui.ReminderItemAdapter
+import com.reminderapp.ui.ReminderAdapter
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit private var reminderAdapter: ReminderItemAdapter
+    lateinit private var reminderAdapter: ReminderAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        reminder_recyclerview.layoutManager = LinearLayoutManager(this)
-        reminderAdapter= ReminderItemAdapter()
-        reminder_recyclerview.adapter = reminderAdapter
-        reminderAdapter.updateItems(DataManager.provideData())
+        reminderAdapter = ReminderAdapter(this, DataManager.provideData())
+        reminder_listview.adapter = reminderAdapter
+        reminder_listview.setOnItemClickListener { parent, view, position, id ->
+            editReminder(view)
+        }
+    }
+
+    private fun editReminder(item: View) {
+
     }
 }
