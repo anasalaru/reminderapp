dc9f0bca1ae6b9b26d60e08ead4378ec56cf1dbd
Ana Maria Salaru <anamaria.salaru@thinslices.com>
2017-12-03 13:12:57 +0200

    Add send as email

--- app/src/main/java/com/reminderapp/activities/EditReminderActivity.kt
+++ app/src/main/java/com/reminderapp/activities/EditReminderActivity.kt
@@ -1,8 +1,11 @@
 package com.reminderapp.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import com.example.anamaria.reminderapp.R
 import com.reminderapp.data.DataManager
@@ -27,5 +30,26 @@ class EditReminderActivity : AppCompatActivity() {
         reminderItem.title = edit_reminder_title.text.toString()
         Log.d("reminderapp", reminderItem.title)
         DataManager.updateItem(reminderItem)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_reminder_item, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.action_send_mail) {
+            sendEmail()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun sendEmail() {
+        val content = "Hey! Don't forget to ${edit_reminder_title.text}. Thanks a lot!"
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc2822"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "ReminderAPP")
+        intent.putExtra(Intent.EXTRA_TEXT, content)
+        startActivity(intent)
     }
 }
