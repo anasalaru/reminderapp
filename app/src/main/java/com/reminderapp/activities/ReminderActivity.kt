37311074acf0616622832c3312fd2eee18bb7dc6
Ana Maria Salaru <anamaria.salaru@thinslices.com>
2017-12-29 13:40:39 +0200

    Rename MainActivity to ReminderActivity and change its package name

--- app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
+++ app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
@@ -1,16 +1,15 @@
-package com.example.anamaria.activities
+package com.reminderapp.activities
 
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.anamaria.reminderapp.R
-import com.reminderapp.activities.EditReminderActivity
 import com.reminderapp.data.DataManager
 import com.reminderapp.models.ReminderItemModel
 import com.reminderapp.ui.ReminderAdapter
 import kotlinx.android.synthetic.main.activity_main.reminder_listview
 
-class MainActivity : AppCompatActivity() {
+class ReminderActivity : AppCompatActivity() {
 
     lateinit private var reminderAdapter: ReminderAdapter
 
