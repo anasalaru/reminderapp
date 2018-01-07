a755faeb3eda89d488267c5b92afbfacd412acc6
Ana Maria Salaru <anamaria.salaru@thinslices.com>
2018-01-07 12:45:47 +0200

    Revert "Remove EditReminderActivity, add fragments"
    This reverts commit e5b4317e07b5e96a71ca85fe6ae0aef9bff80b73.

--- app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
+++ app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
@@ -1,63 +1,38 @@
 package com.reminderapp.activities
 
-import android.app.Fragment
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.example.anamaria.reminderapp.R
-import com.reminderapp.fragments.EditReminderFragment
-import com.reminderapp.fragments.ReminderItemsFragment
+import com.reminderapp.data.DataManager
 import com.reminderapp.models.ReminderItemModel
+import com.reminderapp.ui.ReminderAdapter
+import kotlinx.android.synthetic.main.activity_main.reminder_listview
 
 class ReminderActivity : AppCompatActivity() {
 
-    private val TAG = "REMINDERITEMSFRAGMENT"
+    lateinit private var reminderAdapter: ReminderAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        addFragment(ReminderItemsFragment.newInstance(), false)
-        Log.d(TAG, "Activity -> onCreate")
-    }
 
-    private fun addFragment(fragment: Fragment, replace: Boolean) {
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        if(replace)
-            fragmentTransaction.replace(R.id.main_container, fragment)
-        else
-            fragmentTransaction.add(R.id.main_container, fragment)
-        fragmentTransaction.addToBackStack(null)
-        fragmentTransaction.commit()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "Activity -> onStart")
-
+        reminderAdapter = ReminderAdapter(this, DataManager.provideData())
+        reminder_listview.adapter = reminderAdapter
+        reminder_listview.setOnItemClickListener { parent, view, position, id ->
+            editReminder(parent.adapter.getItem(position))
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "Activity -> onResume")
+
+       reminderAdapter.notifyDataSetChanged()
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "Activity -> onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "Activity -> onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "Activity -> onDestroy")
-    }
-
-    fun itemClicked(item: ReminderItemModel) {
-        Log.d("test", "item clicked")
-        addFragment(EditReminderFragment.newInstance(item), true)
+    private fun editReminder(item: Any) {
+        val intent = Intent(this, EditReminderActivity::class.java)
+        intent.putExtra("reminder", (item as ReminderItemModel))
+        startActivity(intent)
     }
 }
