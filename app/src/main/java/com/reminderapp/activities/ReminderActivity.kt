429b067268512ba88249f06f749c4d005aa9f739
Ana Maria Salaru <anamaria.salaru@thinslices.com>
2018-01-24 14:49:55 +0200

    Remove unneeded code

--- app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
+++ app/src/main/java/com/reminderapp/activities/ReminderActivity.kt
@@ -3,7 +3,6 @@ package com.reminderapp.activities
 import android.app.Fragment
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.example.anamaria.reminderapp.R
 import com.reminderapp.fragments.EditReminderFragment
 import com.reminderapp.fragments.ReminderItemsFragment
@@ -17,7 +16,6 @@ class ReminderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         addFragment(ReminderItemsFragment.newInstance(), false)
-        Log.d(TAG, "Activity -> onCreate")
     }
 
     private fun addFragment(fragment: Fragment, replace: Boolean) {
@@ -30,34 +28,7 @@ class ReminderActivity : AppCompatActivity() {
         fragmentTransaction.commit()
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "Activity -> onStart")
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "Activity -> onResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "Activity -> onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "Activity -> onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "Activity -> onDestroy")
-    }
-
     fun itemClicked(item: ReminderItemModel) {
-        Log.d("test", "item clicked")
         addFragment(EditReminderFragment.newInstance(item), true)
     }
 }
